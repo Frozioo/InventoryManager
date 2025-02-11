376de33a3fb4615c830a8067ba33e9e0de96d3d7
Frozioo <tml100@live.com>
2025-02-11 06:27:26 -0600

    minor additions/information

--- Pages/index.php
+++ Pages/index.php
@@ -1,6 +1,24 @@
+<!-- 
+This will be the main page for inventory manager (subject to change). The user will be able to sign up and login here.
+ There will also be additional information about the site and its purpose. 
+-->
+
 <?php
     // Needed for MariaDB to work with PHP
     require '/home/tmlarson/connections/connect.php';
 
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory Manager</title>
+</head>
+<body>
+    
+</body>
+</html>
