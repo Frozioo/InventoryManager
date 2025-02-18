e3f3c7a1ca90177c8f42b4b1550202f4f0173197
Frozioo <tml100@live.com>
2025-02-18 04:31:32 -0600

    update dashboard title and add TODO list for inventory features

--- dashboard.php
+++ dashboard.php
@@ -21,7 +21,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/stylesheet.css">
-    <title>Document</title>
+    <title>Dashboard - Inventory Manager</title>
 </head>
 <body>
 
